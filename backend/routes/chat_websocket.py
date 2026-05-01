1c1d2f404377795ea4b6ac10b06358bbfb314c65
juliand924 <julianemanuel.drexler@uzh.ch>
2026-05-01 23:46:23 +0200

    updated websocket script to asynchronously submit chat state

--- backend/routes/chat_websocket.py
+++ backend/routes/chat_websocket.py
@@ -1,7 +1,6 @@
-from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.memory import MemorySaver
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 from langgraph.types import Command
-import sqlite3
 from langchain_ollama import ChatOllama
 from workflows.smart_feedback.workflow import SmartFeedbackWorkflow
 import uuid 
@@ -10,50 +9,62 @@ import uuid
 
 router = APIRouter(prefix="/ws", tags=["chat"])
 
-# Thread-safe SQLite
-checkpointer = SqliteSaver(
-    sqlite3.connect(":memory:", check_same_thread=False)
-)
 
 
 @router.websocket("/chat")
 async def websocket_endpoint(websocket: WebSocket):
+    initial_state = {
+            "user_query": "",
+            "chat_history": [],
+            "needs_clarification": False,
+            "human_assessment": "",
+            "ticket_id": "",
+            "analysis_agent_result": None,
+            "rag_results": [],
+            "rag_workflow_state": {},
+            "triage_workflow_state": {},
+            "engagement_response": "",
+        }
     chat_model = ChatOllama(model="hf.co/unsloth/granite-4.0-h-tiny-GGUF:Q8_0")
     config = {"configurable": {"thread_id": str(uuid.uuid4())}}
 
     await websocket.accept()
+    # Thread-safe SQLite
+    checkpointer = MemorySaver()
+    
+    workflow = SmartFeedbackWorkflow(chat_model, checkpointer=checkpointer, graph_config=config)
+    should_run: bool = True
+    user_input = None
+    graph_input = None
 
     try:
-        workflow = SmartFeedbackWorkflow(chat_model, checkpointer=checkpointer)
-        result = workflow.run(
-            user_query="",
-            is_first_message=True,
-            config=config
-        )
+        while should_run:
 
-        # Handle interrupts
-        while True:
-            await websocket.send_text(f"{result}")
-
-            if "__interrupt__" in result:
-                # Send the interrupt message to the user
-                for msg in result["__interrupt__"]:
-                    await websocket.send_text(f"{result}")
-
-                # Wait for user response
-                response = await websocket.receive_text()
-                result = workflow.workflow.invoke(
-                    Command(resume= response),  # Resume with user input
-                    config=config
-                )
+            if user_input: 
+                graph_input = user_input
             else:
-                # Workflow complete
-                break
-            
+                graph_input = initial_state
+            async for chunk in workflow.workflow.astream(
+                graph_input,
+                config=config,
+                version="v2",
+                debug=True
+            ):  
+                await websocket.send_text(f"{chunk}")
+                if chunk["type"] == "updates":
+                    if "__interrupt__" in chunk["data"]:
+
+                        response = await websocket.receive_text()
+                        user_input = Command(resume=response)
+
+            if "end_node" in chunk["data"]:
+                should_run = False            
 
     except WebSocketDisconnect:
         print("Client disconnected")
     except Exception as e:
         print(f"Error: {e}")
+        import traceback
+        traceback.print_exc()
         await websocket.close()
     await websocket.close()
